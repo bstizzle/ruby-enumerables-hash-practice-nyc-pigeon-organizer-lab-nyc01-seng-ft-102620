8a19974c94d406010a4eb4934321677144f0a002
Benjamin Stein <whereyouben@gmail.com>
2020-10-21 01:36:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      }
+      
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_data = data.each_with_object({}) do |(k, v), re_data| 
+    v.each do |k2, v2|
+      v2.each do |name|
+        if !re_data[name]
+          re_data[name] = {}
+        end 
+      end 
+    end 
+  end 
 end
