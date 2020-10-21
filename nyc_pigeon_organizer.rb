340c41b4709859a511473717bbd450fb002b8898
Benjamin Stein <whereyouben@gmail.com>
2020-10-21 01:53:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,7 +29,7 @@ def nyc_pigeon_organizer(data)
         if !re_data[name][k]
           re_data[name][k] = []
         end 
-        re_data[name][key] << v2
+        re_data[name][k] << v2
       end
     end 
   end 
